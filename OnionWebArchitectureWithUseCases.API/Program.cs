336746ee7cc8bec0bf3e0916348d71ba22672fd0
R1ck-C137 <fix2605@gmail.com>
2025-02-24 16:47:33 +0100

    Remove Validation

--- OnionWebArchitectureWithUseCases.API/Program.cs
+++ OnionWebArchitectureWithUseCases.API/Program.cs
@@ -1,8 +1,6 @@
 using FluentValidation;
 using MediatR;
-using OnionWebArchitectureWithUseCases.Application.Behaviors;
 using OnionWebArchitectureWithUseCases.Application.Clients.Command;
-using OnionWebArchitectureWithUseCases.Application.Clients.Validations;
 using OnionWebArchitectureWithUseCases.Core.Stores;
 using OnionWebArchitectureWithUseCases.Endpoints;
 using OnionWebArchitectureWithUseCases.Persistence;
@@ -21,12 +19,10 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
-        builder.Services.AddValidatorsFromAssemblyContaining<CreateClientCommandValidator>();
         builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(
             typeof(CreateClientCommand).Assembly
         ));
         builder.Services.AddScoped<IClientStore, ClientRepository>();
-        builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
         //builder.Services.AddScoped<IMailService, MailService>();
         
         
